c5421a4a2f546d03449c0cdd28a21f209c05e7b5
qthang02 <nguyenquocthang909@gmail.com>
2023-11-12 10:06:06 +0700

    adding image service

--- server/Program.cs
+++ server/Program.cs
@@ -39,6 +39,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 builder.Services.AddAuthorization();
 builder.Services.AddScoped<TokenService>();
+builder.Services.AddScoped<ImageService>();
+
 
 var app = builder.Build();
 
