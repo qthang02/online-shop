ae20787f110e7051ca944da57f74ef4aad9a1108
qthang02 <nguyenquocthang909@gmail.com>
2023-11-12 08:52:09 +0700

    refactor project

--- server/DTOs/RegisterDto.cs
+++
@@ -1,8 +0,0 @@
-namespace DTOs;
-
-public class RegisterDto
-{
-    public string? Username { get; set; }
-    public string? Email { get; set; }
-    public string? Password { get; set; }
-}
--- server/Program.cs
+++ server/Program.cs
@@ -1,27 +1,18 @@
-using Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ShopDataContext>(options =>
-    options.UseSqlite(connectionString));
+// builder.Services.AddDbContext<>(options =>
+//     options.UseSqlite(connectionString));
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-
-    DataSeeder.Initialize(services);
-}
 
 
 // Configure the HTTP request pipeline.
