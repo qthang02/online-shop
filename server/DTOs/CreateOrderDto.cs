39ad0a6e642c159d797d102201a0940ad4ed7aaf
qthang02 <nguyenquocthang909@gmail.com>
2023-11-12 09:09:41 +0700

    adding dtos

---
+++ server/DTOs/CreateOrderDto.cs
@@ -0,0 +1,10 @@
+using Entities.OrderAggregate;
+
+namespace DTOs
+{
+    public class CreateOrderDto
+    {
+        public bool SaveAddress { get; set; }
+        public ShippingAddress ShippingAddress { get; set; }
+    }
+}
